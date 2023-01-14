7ddbf9e1c21f610b69d056d542640107ca778825
giemaz <ghaleonlt@gmail.com>
2023-01-14 12:20:01 +0200

    data folder and shop data added

---
+++ js/components/data/shopData.js
@@ -0,0 +1,40 @@
+const shopData = [
+    {
+        title: 'Economy',
+        description: 'For stingy people',
+        perks: [
+            'Secure Online Transfer',
+            'Limited Styles for interface',
+            'Bare bones off our stuff',
+        ],
+    },
+    {
+        title: 'Business',
+        description: 'For normies',
+        perks: [
+            'Secure Online Transfer',
+            'Unlimited Styles for interface',
+            'Extra attention for you',
+        ],
+    },
+    {
+        title: 'Premium',
+        description: 'For bonus loving people',
+        perks: [
+            'Secure Online Transfer',
+            'Unlimited Styles for interface',
+            'Your Wallet Loving Attitude',
+        ],
+    },
+    {
+        title: 'Exclusive',
+        description: 'For show-offs',
+        perks: [
+            'Secure Online Transfer',
+            'Unlimited Styles for interface',
+            'Bling and Swag',
+        ],
+    },
+];
+
+export { shopData };
