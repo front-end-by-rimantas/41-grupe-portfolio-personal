1b66377fc7c59f0460aa8c1d01905ac7b1a45822
giemaz <ghaleonlt@gmail.com>
2023-01-15 22:54:39 +0200

    labels data created

---
+++ js/data/labelsData.js
@@ -0,0 +1,21 @@
+const labels = [
+    {
+        brand: './img/brands/brand1.png',
+    },
+    {
+        brand: './img/brands/brand2.png',
+    },
+    {
+        brand: './img/brands/brand3.png',
+    },
+    {
+        brand: './img/brands/brand4.png',
+    },
+    {
+        brand: './img/brands/brand5.png',
+    },
+];
+
+const labels3 = [...labels, ...labels, ...labels];
+
+export { labels3 };
